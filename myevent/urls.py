147ec7e671e54a273d8aa341c932ac8f5080043d
Krish <krishna.sunuwar@gmail.com>
2015-07-19 14:34:49 +0545

    url fix

--- myevent/urls.py
+++ myevent/urls.py
@@ -19,15 +19,4 @@ from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^$', 'events.views.home', name='home'),
-    url(r'^events/$', 'events.views.list', name='event_list'),
-    url(r'^events/(?P<id>\d+)/$', 'events.views.detail', name='event_detail'),
-
-    url(r'^events/join/(?P<event_id>\d+)/$', 'events.views.join', name='event_join'),
-    url(r'^events/cancel/(?P<event_id>\d+)/$', 'events.views.cancel', name='event_cancel'),
-    url(r'^user_event/(?P<user_id>\d+)/$', 'events.views.user_event', name='user_event'),
-
-    url(r'^register/$', 'events.views.register', name='register'),
-    url(r'^login/$', auth_view.login, name='login', kwargs={'template_name': 'user/login.html'}),
-    url(r'^logout/$', auth_view.logout, name='logout', kwargs={'next_page': '/'}),
 ]
