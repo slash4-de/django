98a2bf3e65c2deae5cffd43afc3852de6cc26ce1
Krish <krishna.sunuwar@gmail.com>
2015-07-20 16:30:20 +0545

    model added

--- events/models.py
+++ events/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(default='')
+    venue = models.CharField(max_length=100)
+    seats = models.IntegerField()
+    amount = models.DecimalField(
+        max_digits=11,
+        decimal_places=2
+    )
+    date = models.DateTimeField()
