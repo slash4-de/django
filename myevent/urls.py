869e99293977a5bb8f9a8245701363486f9ffba6
Krish <krishna.sunuwar@gmail.com>
2015-07-21 18:01:34 +0545

    login

--- myevent/urls.py
+++ myevent/urls.py
@@ -23,5 +23,6 @@ urlpatterns = [
     url(r'^events/$', 'events.views.list', name='event_list'),
     url(r'^events/(?P<id>\d+)/$', 'events.views.detail', name='event_detail'),
 
-    url(r'^register/$', 'events.views.register', name='register')
+    url(r'^register/$', 'events.views.register', name='register'),
+    url(r'^login/$', auth_view.login, name='login', kwargs={'template_name': 'users/login.html'}),
 ]
