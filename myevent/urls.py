9fb62ea7cffb5a5baa09ced3baabca42b53ed62a
Krish <krishna.sunuwar@gmail.com>
2015-07-21 18:08:06 +0545

    url

--- myevent/urls.py
+++ myevent/urls.py
@@ -15,7 +15,6 @@ Including another URLconf
 """
 from django.conf.urls import include, url
 from django.contrib import admin
-from django.contrib.auth import views as auth_view
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
