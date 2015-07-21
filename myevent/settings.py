7c6d9934e93f4267c1cb9c8aee93566efd3fc972
Krish <krishna.sunuwar@gmail.com>
2015-07-21 15:55:46 +0545

    settings

--- myevent/settings.py
+++ myevent/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'myevent.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'template', 'html')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -83,10 +83,6 @@ DATABASES = {
     }
 }
 
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'template', 'static'),
-)
 
 
 # Internationalization
